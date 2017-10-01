7371fcec06e6a8302e36137063940c7cfbf5d128
Akshay Nair <phenax5@gmail.com>
2017-10-01 17:12:32 +0530

    Added basic recipies

--- client/js/serviceworker.js
+++ client/js/serviceworker.js
@@ -1,2 +1,45 @@
 
-console.log(self.STATIC_BASE_URL, self.STATIC_CACHE_VERSION);
+import sw from 'sw-toolbox';
+
+const BASE_URL = self.STATIC_BASE_URL;
+const SCRIPT_CACHE_NAME = `diary-cache-js--${self.STATIC_CACHE_VERSION}`;
+const CSS_CACHE_NAME = `diary-cache-css--${self.STATIC_CACHE_VERSION}`;
+const IMG_CACHE_NAME = `diary-cache-images--${self.STATIC_CACHE_VERSION}`;
+
+const NETWORK_TIMEOUT = 5;
+
+
+const precacheFiles = [
+	'js/1.1.js',
+	'js/2.2.js',
+	'js/3.3.js',
+	'js/4.4.js',
+	'js/5.5.js',
+].map(file => BASE_URL + file);
+
+
+sw.precache(precacheFiles);
+
+sw.router.get(/\.js$/, sw.fastest, {
+	networkTimeoutSeconds: NETWORK_TIMEOUT,
+	cache: {
+		name: SCRIPT_CACHE_NAME,
+	},
+});
+
+sw.router.get(/\.(css|woff|woff2|ttf)$/, sw.cacheFirst, {
+	networkTimeoutSeconds: NETWORK_TIMEOUT,
+	cache: {
+		name: CSS_CACHE_NAME,
+	},
+});
+
+sw.router.get(/\.(png|ico|jpg)$/, sw.cacheFirst, {
+	networkTimeoutSeconds: NETWORK_TIMEOUT,
+	cache: {
+		name: IMG_CACHE_NAME,
+	},
+});
+
+
+
